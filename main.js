210f64105aa1bc4bfd59c017af3004563a93ec33
childrem <38900005+childrem@users.noreply.github.com>
2018-11-09 12:01:32 -0800

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 9730);
@@ -15,8 +19,30 @@ app.get('/',function(req,res){
   // array now has all name value pairs from the query string in objects
   var context = {};
   context.queryList = listToSend;
-  context.type = "GET";
+  context.type = "GET"; // Display GET at the top of the page
   res.render('result', context)
+});
+
+app.post('/',function(req,res){
+  var queryStringList = [];
+  for (var p in req.query) {
+    queryStringList.push({'name': p, 'value': req.query[p]});
+  }
+  
+  // queryStringList has any data that happened to come through in the URL query string
+  
+  var bodyList = [];
+  for (var p in req.body) {
+    bodyList.push({'name': p, 'value': req.body[p]});
+  }
+  
+  // bodyList has all data sent through in the request body
+  
+  var context = {};
+  context.queryList = queryStringList;
+  context.bodyList = bodyList;
+  context.type = "POST";
+  res.render('result', context);
 });
 
 app.use(function(req,res){
