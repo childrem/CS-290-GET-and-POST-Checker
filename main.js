c1c33b251d564308e14f8c53e1256cff4a2c23d7
childrem <38900005+childrem@users.noreply.github.com>
2018-11-09 11:07:45 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,35 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 9730);
+
+app.get('/',function(req,res){
+  var listToSend = [];
+  for (var p in req.query) {
+    listToSend.push({'name': p, 'value': req.query[p]});
+  }
+  // array now has all name value pairs from the query string in objects
+  var context = {};
+  context.queryList = listToSend;
+  res.render('result', context)
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://flip2.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
